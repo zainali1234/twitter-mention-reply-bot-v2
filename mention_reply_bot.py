7de67ab19efc21d813ddb6cc18dcc4dc0f27e3e7
zainali1234 <67614246+zainali1234@users.noreply.github.com>
2022-05-16 22:04:21 -0500

    Update mention_reply_bot.py

--- mention_reply_bot.py
+++ mention_reply_bot.py
@@ -8,15 +8,16 @@ ACCESS_TOKEN_SECRET = '**********'
 BEARER_TOKEN = '**********'
 USER_ID = '**********'
 
-client = tweepy.Client(bearer_token=BEARER_TOKEN, consumer_key=API_KEY,
+client = tweepy.Client(bearer_token=BEARER_TOKEN, consumer_key=API_KEY, 
                        consumer_secret=API_KEY_SECRET,
                        access_token=ACCESS_TOKEN,
                        access_token_secret=ACCESS_TOKEN_SECRET)
 
-MENTIONIDLIST = []
+MENTIONIDLIST = [] #Stores previous mention tweets that have been replied to already
 
-def reply_to_tweet():
-    mentions = client.get_users_mentions(id=USER_ID)
+def reply_to_tweet(): #checks if mention tweet has already been replied to, if not, then it replies to tweet and stores it
+                      #in list
+    mentions = client.get_users_mentions(id=USER_ID) 
 
     for mention in mentions.data:
         if mention.id not in MENTIONIDLIST:
